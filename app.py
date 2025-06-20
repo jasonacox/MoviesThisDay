9b289ca36ac4fcf0537dac924a52d3e9371718fa
Jason Cox <jason@jasonacox.com>
2025-06-20 00:42:37 -0700

    Bump version to 0.1.7

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ from fastapi import FastAPI, HTTPException, Query, Request, Form, Body, status
 from fastapi.responses import HTMLResponse, JSONResponse, PlainTextResponse
 from fastapi.templating import Jinja2Templates
 
-VERSION = "0.1.6"
+VERSION = "0.1.7"
 
 POPULARITY_THRESHOLD = 10  # Only show movies with popularity above this value
 AGE_LIMIT = 100  # Only show movies released within this many years
